3adbb9b8d9b53c77cdac3dc30d4758e789d7bb12
Aldair Cruz Bautista <aldair.cruzbautista@outlook.com>
2025-01-28 22:15:31 -0600

    Variables con const

---
+++ js/02.js
@@ -0,0 +1,12 @@
+// Variables con Const
+
+const cliente = "Aldair"
+
+// No se puede reasignar su valor
+/*cliente = 20;*/
+
+console.log(cliente)
+
+// Las variables con const siempre tienen que tener un valor
+const precio = 300;
+console.log(precio);
