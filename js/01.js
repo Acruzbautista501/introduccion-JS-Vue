e1d075c70499a70fd2db2815c66deb870bfbc0b2
Aldair Cruz Bautista <aldair.cruzbautista@outlook.com>
2025-01-28 21:59:38 -0600

    Variables con let

---
+++ js/01.js
@@ -0,0 +1,18 @@
+// Variables con Let
+
+let cliente = "Aldair";
+let precio_descuento = 220;
+
+// Se puede reasignar
+cliente = 20;
+cliente = true;
+
+// Pueden iniciar sin un valor
+let precio;
+
+precio = 300;
+
+console.log(cliente);
+console.log(precio);
+
+
