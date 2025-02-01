041dad78c0bc4dcd3b3145a42327d34f21ede75a
Aldair Cruz Bautista <aldair.cruzbautista@outlook.com>
2025-01-31 23:11:58 -0600

    Introducción a Arrays

---
+++ js/09.js
@@ -0,0 +1,19 @@
+// Arrays o Arreglos 
+/* Los arrays al igual que los objetos, pueden mantener o pueden almacenar mucha 
+información y puedes tener un array de objetos y puedes tener un objeto con arrays, 
+entonces son intercambiables.*/
+/* Los arreglos inician con corchetes[], siendo este un indicativo en JavaScript de 
+que es un arreglo.*/
+/* Los arreglos tienen un valor y el indice se calcula en automático */
+const tecnologias = ['Vue.js', 'JavaScript', 'Node.js', 30, 20, true, {}] // Las comas añaden un nuevo elemento al arreglo
+/*Los arreglos inician a contarse desde la posición cero*
+/* Tambień es útil usar console.table() para mostrar los arreglos */
+
+console.table(tecnologias)
+/* Para acceder a los valores del array tienes que utilizar corchetes[] seguido 
+ de su indice, ya sea 0, 1, o cualquier otro*/
+console.log(tecnologias[1])
+/* Lo ideal, es que tus arreglos utilicen una estructura o sigan una estructura determinada,
+es decir, datos relacionados */
+/* Los arreglos tienen una serie de métodos y funciones que nos permiten ver los elementos 
+del mismo*/
